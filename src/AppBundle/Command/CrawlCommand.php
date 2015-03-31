1aeb4280848ed07b05f7eb7622f5ffabbad905c3
theo <theo.gilcer@gmail.com>
2015-03-31 10:57:36 +0100

    Check for imdbId before creating torrent

--- src/AppBundle/Command/CrawlCommand.php
+++ src/AppBundle/Command/CrawlCommand.php
@@ -22,10 +22,6 @@ class CrawlCommand extends ContainerAwareCommand {
                 ->setDescription('Crawl kickass.to for new content to download!');
     }
 
-    protected function getLinks() {
-        
-    }
-
     protected function execute(InputInterface $input, OutputInterface $output) {
         $client = new Client();
         $client->getCLient()->setDefaultOption('config/curl/' . CURLOPT_SSL_VERIFYPEER, false);
@@ -38,7 +34,7 @@ class CrawlCommand extends ContainerAwareCommand {
         $filmRepo = $doctrine->getRepository("AppBundle:Film");
 
         $crawler->filter('a.cellMainLink')->reduce(function($node, $i) {
-            return $i < 20;
+            return $i < 10;
         })->each(function ($node) use ($client, $torrentRepo, $filmRepo, $manager) {
 
             // RECUPERE LE LIEN VERS LA PAGE "DETAIL DU TORRENT"
@@ -46,95 +42,96 @@ class CrawlCommand extends ContainerAwareCommand {
             $crawler = $client->request('GET', $link->getUri());
             $magnetLink = $crawler->filter('.magnetlinkButton')->first()->attr('href');
             $hash = preg_match("/btih:([0-9a-zA-Z]*)/", $magnetLink, $matches);
-            $hash = $matches[1];
-            $torrentAlreadyExists = $torrentRepo->findOneByHash($hash);
+            $torrentAlreadyExists = $torrentRepo->findOneByHash($matches[1]);
 
             if (!$torrentAlreadyExists) {
-
-                $newTorrent = new Torrent();
-
-                // TITLE
-                $torrentTitle = $crawler->filter('.novertmarg')->first();
-                $newTorrent->setTitle($torrentTitle->text());
-
-                // MAGNET
-                $newTorrent->setMagnet($magnetLink);
-
-                // HASH
-                $newTorrent->setHash($hash);
-
-                // SEEDERS
-                $seeders = $crawler->filter('[itemprop="seeders"]')->first()->text();
-                $newTorrent->setSeeders($seeders);
-
-                // LEECHERS
-                $leechers = $crawler->filter('[itemprop="leechers"]')->first()->text();
-                $newTorrent->setLeechers($leechers);
-
-                //QUALITY
-                $quality = $crawler->filter('[id^="quality"]');
-                if (count($quality) > 0) {
-                    $newTorrent->setQuality($quality->first()->text());
-                };
-
-                $manager->persist($newTorrent);
-
                 //IMDB
                 $imdbId = $crawler->filter('a[href*="http://www.imdb.com/title/tt"]');
                 if (count($imdbId) > 0) {
+                    
+                    $newTorrent = new Torrent();
+                    
                     $imdbId = $imdbId->first()->text();
+                    
                     $film = $filmRepo->findOneByImdbId($imdbId);
+                    
                     if ($film) {
                         $newTorrent->setFilm($film);
                     } else {
-                        $crawler = $client->request('GET', "http://www.imdb.com/title/tt" . $imdbId);
+                        $imdbCrawler = $client->request('GET', "http://www.imdb.com/title/tt" . $imdbId);
 
                         $newFilm = new Film();
 
                         $newFilm->setImdbId($imdbId);
 
                         // Title
-                        $title = $crawler->filter('[itemprop="name"]');
+                        $title = $imdbCrawler->filter('[itemprop="name"]');
                         if( count( $title ) > 0 ) {
                             $newFilm->setTitle( $title->first()->text() );
                         }
                         
                         // DATE
-                        $date = $crawler->filter('[itemprop="datePublished"]');
+                        $date = $imdbCrawler->filter('meta[itemprop="datePublished"]');
                         if(count($date)>0){
                             $date = $date->first()->text();
                         }
 
                         // DIRECTOR
-                        $director = $crawler->filter('[itemprop="director"] [itemprop="name"]');
+                        $director = $imdbCrawler->filter('[itemprop="director"] [itemprop="name"]');
                         if( count( $director ) > 0 ){
                             $newFilm->setDirector($director->first()->text());
                         }
                         
 
                         // THUMB
-                        $thumb = $crawler->filter('[itemprop="image"]');
+                        $thumb = $imdbCrawler->filter('[itemprop="image"]');
                         if (count($thumb) > 0) {
                             $thumbSrc = $thumb->first()->attr("src");
                             $newFilm->setThumbnail($thumbSrc);
                         }
 
                         // SCORE
-                        $rating = $crawler->filter('[itemprop="ratingValue"]');
+                        $rating = $imdbCrawler->filter('[itemprop="ratingValue"]');
                         if( count( $rating ) > 0 ){
                             $newFilm->setRating( $rating->first()->text() );
                         }
                         
                         // VOTES
-                        $votes = $crawler->filter('[itemprop="ratingCount"]');
+                        $votes = $imdbCrawler->filter('[itemprop="ratingCount"]');
                         if( count( $votes ) > 0 ){
                             $newFilm->setVotes($votes->first()->text());
                         }
                         
-                        $newTorrent->setFilm($newFilm);
-
                         $manager->persist($newFilm);
+                        
+                        $newTorrent->setFilm($newFilm);
                     }
+
+                    // TITLE
+                    $torrentTitle = $crawler->filter('.novertmarg')->first();
+                    $newTorrent->setTitle($torrentTitle->text());
+
+                    // MAGNET
+                    $newTorrent->setMagnet($magnetLink);
+
+                    // HASH
+                    $newTorrent->setHash($hash);
+
+                    // SEEDERS
+                    $seeders = $crawler->filter('[itemprop="seeders"]')->first()->text();
+                    $newTorrent->setSeeders($seeders);
+
+                    // LEECHERS
+                    $leechers = $crawler->filter('[itemprop="leechers"]')->first()->text();
+                    $newTorrent->setLeechers($leechers);
+
+                    //QUALITY
+                    $quality = $crawler->filter('[id^="quality"]');
+                    if (count($quality) > 0) {
+                        $newTorrent->setQuality($quality->first()->text());
+                    };
+
+                    $manager->persist($newTorrent);
                 };
             }
         });
